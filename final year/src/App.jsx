a25d8683fb738050b28966c6d40a506f9373e48a
piyushpotghan <piyusjpotghan200724>
2025-07-16 16:39:07 +0530

    initial commit

---
+++ final year/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-blue-300  flex items-center justify-center text-black-900 p-16'>
+      members welcome to final year journey
+    </div>
+  )
+}
+
+export default App;
