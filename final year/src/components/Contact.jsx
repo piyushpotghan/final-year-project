44e932d6a0b2b514fe819bfa99f5731ef45fc2af
ingoleanushka20 <ingoleanushka20@gmail.com>
2025-07-20 21:50:07 +0530

    ...

--- final year/src/components/Contact.jsx
+++ final year/src/components/Contact.jsx
@@ -63,7 +63,7 @@ export default function Contact() {
           type="submit"
           className="w-full py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
         >
-          Send Message
+          Send Message 
         </button>
       </form>
     </div>
