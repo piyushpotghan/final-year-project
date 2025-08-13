ed67f1ef29163c34831aa8025cb742df508f8000
ingoleanushka20 <ingoleanushka20@gmail.com>
2025-08-13 12:10:58 +0530

    changes done

--- final year/src/doctordash/Header.jsx
+++ final year/src/doctordash/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="flex justify-between items-center p-4 border-b bg-white">
       <div className="text-gray-700 font-semibold"></div>
       <button className="px-4 py-1 bg-blue-500 text-white rounded-full hover:bg-blue-600">
-     
+        
       </button>
     </div>
   );
